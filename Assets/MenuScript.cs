863d2e1ea44156f854a024c7092a439ca7236d9e
ANDREW\yuiweit <yuiweit@andrew.cmu.edu>
2019-01-26 15:44:02 -0500

    unity stuff

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField] Image credits;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit ();
+        #endif
+    }
+
+    public void OpenCredits()
+    {
+        credits.gameObject.SetActive(true);
+    }
+    public void CloseCredits()
+    {
+        credits.gameObject.SetActive(false);
+    }
+ 
+}

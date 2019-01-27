479dcd4c5b68a72747867c9570049586f537d18c
ANDREW\yuiweit <yuiweit@andrew.cmu.edu>
2019-01-26 20:45:32 -0500

    main menu assets

---
+++ Assets/MenuAssets/script/Floating.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    [SerializeField] Transform title;
+    int dir = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("FlipDirection", 0f, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(title.position, dir * Vector3.up, 5 * Time.deltaTime);
+    }
+
+    void FlipDirection()
+    {
+        dir *= -1;
+    }
+}
---
+++ Assets/MenuAssets/script/StartButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    private void OnMouseDown()
+    {
+        LoadByIndex(1);
+    }
+
+}
